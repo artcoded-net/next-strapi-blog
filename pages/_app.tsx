b613fbaa24f556cefb1509af145ebb38095a7096
Artcoded <dev@artcoded.net>
2022-03-11 16:54:02 +0100

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import StrapiApolloProvider from '../graphql/apollo'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <StrapiApolloProvider><Component {...pageProps} /></StrapiApolloProvider>
 }
 
 export default MyApp
