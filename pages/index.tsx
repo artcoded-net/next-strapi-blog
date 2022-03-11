2179ffb165a0da4e56b30c6051fe7648376f8638
Artcoded <dev@artcoded.net>
2022-03-11 16:53:49 +0100

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,28 @@
+import { useQuery } from '@apollo/client'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { PostsDocument, PostsQuery, PostsQueryVariables } from '../generated'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+
+  const { data, error, loading } = useQuery<PostsQuery, PostsQueryVariables>(PostsDocument);
+
+  if(loading) return (
+    <main className={styles.main}>
+        Loading...
+    </main>
+  );
+
+  if(error) return (
+    <main className={styles.main}>
+      {error.message}
+    </main>
+  )
+
+  // If we're here, we have some data in our data variable
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,44 +32,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {data}
       </main>
 
       <footer className={styles.footer}>
